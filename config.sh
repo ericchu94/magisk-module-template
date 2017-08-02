d5ad08d9749e13c9f7a8f2220e04b397837c8c02
Eric Chu <eric@ericchu.net>
2017-08-01 23:01:14 -0400

    Add splatoon 2 font module

--- config.sh
+++ config.sh
@@ -25,7 +25,7 @@
 
 # This will be the folder name under /magisk
 # This should also be the same as the id in your module.prop to prevent confusion
-MODID=template
+MODID=ericchu-splatoon-2-font
 
 # Set to true if you need to enable Magic Mount
 # Most mods would like it to be enabled
@@ -47,9 +47,9 @@ LATESTARTSERVICE=false
 # Set what you want to show when installing your mod
 
 print_modname() {
-  ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
-  ui_print "*******************************"
+  ui_print "*************************"
+  ui_print "     Splatoon 2 Font     "
+  ui_print "*************************"
 }
 
 ##########################################################################################
